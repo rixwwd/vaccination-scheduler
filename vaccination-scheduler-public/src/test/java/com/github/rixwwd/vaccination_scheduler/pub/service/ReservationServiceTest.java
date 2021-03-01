374a21fe02e5727543d2201749513a8c39c5aef2
rixwwd <rixwwd@users.noreply.github.com>
2021-03-02 00:24:37 +0900

    テスト作成

---
+++ vaccination-scheduler-public/src/test/java/com/github/rixwwd/vaccination_scheduler/pub/service/ReservationServiceTest.java
@@ -0,0 +1,81 @@
+package com.github.rixwwd.vaccination_scheduler.pub.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.github.rixwwd.vaccination_scheduler.pub.entity.Cell;
+import com.github.rixwwd.vaccination_scheduler.pub.entity.Coupon;
+import com.github.rixwwd.vaccination_scheduler.pub.entity.Reservation;
+import com.github.rixwwd.vaccination_scheduler.pub.entity.VaccineStock;
+import com.github.rixwwd.vaccination_scheduler.pub.repository.CellRepository;
+import com.github.rixwwd.vaccination_scheduler.pub.repository.ReservationRepository;
+import com.github.rixwwd.vaccination_scheduler.pub.repository.VaccineStockRepository;
+
+@SpringBootTest
+class ReservationServiceTest {
+
+	@Autowired
+	ReservationService reservationService;
+
+	@MockBean
+	CellRepository cellRepository;
+
+	@MockBean
+	ReservationRepository reservationRepository;
+
+	@MockBean
+	VaccineStockRepository vaccineStockRepository;
+
+	@Test
+	void testReserve() {
+
+		var cellId = UUID.fromString("3b730ff1-cbda-4eeb-83a1-e61ab9c071a7");
+		var roomId = UUID.fromString("c182a523-1256-4621-b3d8-81d96d16f237");
+
+		var cell = new Cell();
+		cell.setId(cellId);
+		cell.setCapacity(1);
+		cell.setRoomId(roomId);
+
+		when(cellRepository.findByIdForWrite(Mockito.any(UUID.class))).thenReturn(Optional.of(cell));
+
+		var stock = new VaccineStock();
+		stock.setQuantity(1);
+		when(vaccineStockRepository.getByRoomIdAndGraterThanEqualExpectedDeliveryDateForWrite(roomId, LocalDate.now()))
+				.thenReturn(Arrays.asList(stock));
+
+		when(reservationRepository.countTodayReservationByRoomId(roomId)).thenReturn(0L);
+
+		when(reservationRepository.countByCellId(cellId)).thenReturn(0L);
+
+		var reservation = new Reservation();
+		reservation.setCellId(cellId);
+		reservation.setCoupon(new Coupon("12345"));
+
+		when(reservationRepository.saveAndFlush(reservation)).thenAnswer(new Answer<Reservation>() {
+
+			@Override
+			public Reservation answer(InvocationOnMock invocation) throws Throwable {
+				return invocation.getArgument(0);
+			}
+		});
+
+		var actualReservation = reservationService.reserve(reservation, new Coupon("12345"));
+
+		assertNotNull(actualReservation.getReservationNumber());
+	}
+
+}
