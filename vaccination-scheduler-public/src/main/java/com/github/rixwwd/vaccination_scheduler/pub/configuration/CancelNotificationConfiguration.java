944c42fb467b9b7df8aedfe9004564bd618c7d4f
rixwwd <rixwwd@users.noreply.github.com>
2021-04-02 22:43:27 +0900

    SMTPサーバーがなくても起動するようにした

--- vaccination-scheduler-public/src/main/java/com/github/rixwwd/vaccination_scheduler/pub/configuration/CancelNotificationConfiguration.java
+++ vaccination-scheduler-public/src/main/java/com/github/rixwwd/vaccination_scheduler/pub/configuration/CancelNotificationConfiguration.java
@@ -1,12 +1,14 @@
 package com.github.rixwwd.vaccination_scheduler.pub.configuration;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import com.github.rixwwd.vaccination_scheduler.pub.repository.WaitingListRepository;
 import com.github.rixwwd.vaccination_scheduler.pub.service.CancelNoticeService;
+import com.github.rixwwd.vaccination_scheduler.pub.service.NoopCancelNoticeService;
 import com.github.rixwwd.vaccination_scheduler.pub.service.SmtpCancelNoticeService;
 
 @Configuration
@@ -14,9 +16,15 @@ public class CancelNotificationConfiguration {
 
 	@Bean
 	@ConditionalOnBean(JavaMailSender.class)
-	public CancelNoticeService cancelNoticeService(JavaMailSender mailSender,
+	public CancelNoticeService smtpCancelNoticeService(JavaMailSender mailSender,
 			WaitingListRepository waitingListRepository) {
 		return new SmtpCancelNoticeService(mailSender, waitingListRepository);
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(JavaMailSender.class)
+	public CancelNoticeService noopCancelNoticeService(WaitingListRepository waitingListRepository) {
+		return new NoopCancelNoticeService(waitingListRepository);
 	}
 
 }
