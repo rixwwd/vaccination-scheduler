61b3e967dce8dadb563634c7b5497f9f13a0759b
rixwwd <rixwwd@users.noreply.github.com>
2021-04-04 18:25:59 +0900

    主キーがなかったのを修正

--- vaccination-scheduler-admin/src/main/resources/db/migration/V2021032901__create_waiting_list.sql
+++ vaccination-scheduler-admin/src/main/resources/db/migration/V2021032901__create_waiting_list.sql
@@ -1,10 +1,12 @@
 create table waiting_list (
-  cell_id uuid not null,
-  public_user_id uuid not null,
+  cell_id uuid,
+  public_user_id uuid,
   created_at timestamp not null,
   
+  primary key(cell_id, public_user_id),
   foreign key (public_user_id) references public_users (id),
   foreign key (cell_id) references cells (id)
 );
 
 create index waiting_list_cell_id on waiting_list (cell_id);
+create index waiting_list_public_user_id on waiting_list (public_user_id);
--- vaccination-scheduler-public/src/main/resources/schema.sql
+++ vaccination-scheduler-public/src/main/resources/schema.sql
@@ -126,13 +126,15 @@ create index coupons_public_user_id on coupons (public_user_id);
 
 -- V2021032901__create_waiting_list.sql
 create table waiting_list (
-  cell_id uuid not null,
-  public_user_id uuid not null,
+  cell_id uuid,
+  public_user_id uuid,
   created_at timestamp not null,
   
+  primary key(cell_id, public_user_id),
   foreign key (public_user_id) references public_users (id),
   foreign key (cell_id) references cells (id)
 );
 
 create index waiting_list_cell_id on waiting_list (cell_id);
+create index waiting_list_public_user_id on waiting_list (public_user_id);
 
